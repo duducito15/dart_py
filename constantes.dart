11e6e921a37a4817fe70dd20ba1184f2e8695c02
Eduardo <echavezv@utea.edu.pe>
2024-11-20 07:59:43 -0500

    constantes

---
+++ constantes.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final int constante1 = 12;
+  print(constante1);
+  print(constante1.toString());
+
+  const List<int> constante2 = [12, 15];
+  constante2.add(18);
+
+  print(constante2);
+}
