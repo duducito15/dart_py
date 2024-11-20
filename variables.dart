585dd4b4e1702965dd0cd0c6963af1caee795c94
Eduardo <echavezv@utea.edu.pe>
2024-11-20 07:40:47 -0500

    Variables numericas

---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  //Los numeros
+  int miEntero = 2;
+  double miReal = 1.256;
+
+  print(miEntero);
+  print(miEntero.toString());
+
+  print(miEntero + miEntero);
+
+  print(miReal.toInt());
+
+  var x = 1;
+  var y = "2";
+  print(x);
+  print(y);
+}
