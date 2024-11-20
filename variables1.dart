c366d566a5265fce159d0a7f4b1f3da031b2c0c4
Eduardo <echavezv@utea.edu.pe>
2024-11-20 07:49:23 -0500

    Variables tipo cadena

---
+++ variables1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //Las cadenas de caracteres
+  String miCadena = 'Holas soy Eduardo';
+  print(miCadena);
+  print(miCadena.length);
+  print(miCadena.toLowerCase());
+  print(miCadena.toUpperCase());
+  print(miCadena.substring(0, 4));
+}
