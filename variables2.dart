5277a4870b7ee22b9c7715c64925c973025e26b0
Eduardo <echavezv@utea.edu.pe>
2024-11-20 07:55:22 -0500

    Tipos dinamicos

---
+++ variables2.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //Los booleanos
+
+  bool opcion1 = true;
+  bool opcion2 = false;
+
+  print(opcion1);
+  print(opcion2);
+}
---
+++ variables3.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //Tipos dinamicos
+  // var y dynamic
+
+  var unaVariale = "Hola";
+  dynamic otraVariable = 1;
+  int numero = 15;
+
+  print(unaVariale);
+  print(otraVariable);
+  print(otraVariable + numero);
+}
