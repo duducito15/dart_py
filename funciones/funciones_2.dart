cdcbfe2ccc804588fef9c9298d3d44821fc44e55
Eduardo <echavezv@utea.edu.pe>
2024-11-25 07:01:12 -0500

    funciones con argumentos

--- funciones/funciones_2.dart
+++ funciones/funciones_2.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  //invocar a la funcion
+  print(nombreMayusculas("ruth"));
+}
+
+String nombreMayusculas(String nombre) {
+  String respuesta = "Hola ${nombre.toUpperCase()}";
+  return respuesta;
+}
+
+void argumnetosOpcionales(String nombre, String apellido, [int? edad]) {
+  if (edad != null) {
+    print("Nombre: $nombre, Apellido: $apellido, Edad: $edad");
+  } else {
+    print("Nombre: $nombre, Apellido: $apellido");
+  }
+}
